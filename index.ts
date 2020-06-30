ff87d46dd3b207bea4ef75a6760316d1f3803549
trinnawat <trinnawat_got@hotmail.com>
2020-06-30 14:29:10 +0700

    2 files modified

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// const appDiv: HTMLElement = document.getElementById('app');
+// appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
